0abb534e418643886bf4da57b3c1f1ad1eefb865
Marco <marco.baresi01@gmail.com>
2025-11-03 20:44:28 +0100

    feat(interpreter): implement basic interpreter to run with all jpamb suite cases

--- solutions/interpreter.py
+++ solutions/interpreter.py
@@ -1,18 +1,52 @@
-import jpamb
-from jpamb import jvm
+"""
+jpamb.jvm.opcode
+
+This module contains the decompilation of the output of jvm2json
+into a python structure, as well documentation and semantics for
+each instruction.
+
+Sample execution:
+- `uv run jpamb interpret --stepwise --filter Simple.divideByN: solutions/interpreter.py`
+"""
+import sys
+
 from dataclasses import dataclass
 
-import sys
 from loguru import logger
 
-logger.remove()
-logger.add(sys.stderr, format="[{level}] {message}")
+import jpamb
+from jpamb.model import Input
+from jpamb import jvm
 
-methodid, input = jpamb.getcase()
+
+def wrap_value(value: any) -> jvm.Value:
+    """Parses the input value to a [jvm.Value] object"""
+    match value:
+        case bool():
+            return jvm.Value.boolean(value)
+        case int():
+            return jvm.Value.int(value)
+        case str() if len(value) == 1:
+            return jvm.Value.char(value)
+        case tuple():
+            # TODO implement for different types, not only int
+            return jvm.Value.array(jvm.Int(), value)
+        case None:
+            return jvm.Value.boolean(False)
+        case _:
+            raise TypeError(f"Do not know how to wrap {value!r}")
 
 
 @dataclass
 class PC:
+    """
+    Program Counter of the program.
+    It contains the Method Id and the Offset.
+    ι = ⟨ι_m, ι_o⟩
+    ι + n = ⟨ι_m, ι_o + n⟩
+    n/ι = ⟨ι_m, n⟩
+    """
+
     method: jvm.AbsMethodID
     offset: int
 
@@ -29,6 +63,11 @@ class PC:
 
 @dataclass
 class Bytecode:
+    """
+    Context (bytecode) of the program.
+    It performs a simple operation bd[ι] which looks up the bytecode
+    instruction at ι.
+    """
     suite: jpamb.Suite
     methods: dict[jvm.AbsMethodID, list[jvm.Opcode]]
 
@@ -41,9 +80,27 @@ class Bytecode:
 
         return opcodes[pc.offset]
 
+    def get_static_field(self, pc: PC, field: jvm.AbsFieldID) -> jvm.Value:
+        """Returns the static field value given PC and field id."""
+        fields = self.suite.findclass(pc.method.classname)["fields"]
+        for f in fields:
+            if f["name"] == field.fieldid.name:
+                return wrap_value(f["value"])
+        raise KeyError(f"Static field not found: {field.fieldid.name}")
+
 
 @dataclass
 class Stack[T]:
+    """
+    The JVM is a stack based machine: it uses an operator stack.
+
+    A stack is a list of values:
+    σ = 𝐕_σ*.
+
+    Each value is defined as follows:
+    𝐕_σ := (int n) | (float f) | (ref r)
+    ('ref' represents a reference to the heap / global memory).
+    """
     items: list[T]
 
     def __bool__(self) -> bool:
@@ -51,99 +108,444 @@ class Stack[T]:
 
     @classmethod
     def empty(cls):
+        """Returns an empty instance of [Stack]."""
         return cls([])
 
     def peek(self) -> T:
+        """Peek the last element of the [Stack]."""
         return self.items[-1]
 
     def pop(self) -> T:
+        """Pops the last element of the [Stack]."""
         return self.items.pop(-1)
 
-    def push(self, value):
+    def push(self, value: T):
+        """Pushes the value in the [Stack]."""
         self.items.append(value)
         return self
 
     def __str__(self):
-        if not self:
-            return "ϵ"
-        return "".join(f"{v}" for v in self.items)
+        return "ϵ" if not self else "".join(f"{v}" for v in self.items)
 
 
-suite = jpamb.Suite()
-bc = Bytecode(suite, dict())
-
-
-@dataclass
+@dataclass  # type: ignore
 class Frame:
+    """
+    The state of the JVM is a triplet:
+    ⟨λ, σ, ι⟩
+
+    Where:
+    - λ is the Locals that stores local variables of type 𝐕_σ
+    - σ is the Operational Stack
+    - ι is the Program Counter
+    """
     locals: dict[int, jvm.Value]
     stack: Stack[jvm.Value]
     pc: PC
 
     def __str__(self):
-        locals = ", ".join(f"{k}:{v}" for k, v in sorted(self.locals.items()))
-        return f"<{{{locals}}}, {self.stack}, {self.pc}>"
+        locals_str = ", ".join(f"{k}:{v}" for k, v in self.locals.items())
+        return f"<{{{locals_str}}}, {self.stack}, {self.pc}>"
 
-    def from_method(method: jvm.AbsMethodID) -> "Frame":
+    @classmethod
+    def from_method(cls, method: jvm.AbsMethodID) -> "Frame":
+        """Returns an empty Frame object from the method id."""
         return Frame({}, Stack.empty(), PC(method, 0))
 
 
-@dataclass
+@dataclass  # type: ignore
 class State:
+    """
+    In the JVM methods are capable of calling other methods.
+    The State needs to be a tuple of the heap and the call stack:
+    ⟨η,μ⟩
+
+    The heap η is the way for frames to share data.
+    The heap is a mapping from memory locations (int) to 𝐕_η (heap values).
+
+    The heap values are defined as follows:
+    𝐕_η := 𝐕_σ | (byte b) | (char c) | (short s) | (array t a) | (object cn fs)
+    """
     heap: dict[int, jvm.Value]
     frames: Stack[Frame]
 
-    def __str__(self):
-        return f"{self.heap} {self.frames}"
 
+def step(state: State, bytecode: Bytecode) -> State | str:
+    """
+    Stepping function:
+    bc ⊢ ⟨η,μ⟩ → ⟨η‾,μ‾⟩
+    """
 
-def step(state: State) -> State | str:
-    assert isinstance(state, State), f"expected frame but got {state}"
     frame = state.frames.peek()
-    opr = bc[frame.pc]
-    logger.debug(f"STEP {opr}\n{state}")
-    match opr:
-        case jvm.Push(value=v):
-            frame.stack.push(v)
-            frame.pc += 1
-            return state
-        case jvm.Load(type=jvm.Int(), index=i):
-            frame.stack.push(frame.locals[i])
-            frame.pc += 1
-            return state
-        case jvm.Binary(type=jvm.Int(), operant=jvm.BinaryOpr.Div):
-            v2, v1 = frame.stack.pop(), frame.stack.pop()
-            assert v1.type is jvm.Int(), f"expected int, but got {v1}"
-            assert v2.type is jvm.Int(), f"expected int, but got {v2}"
-            if v2.value == 0:
-                return "divide by zero"
 
-            frame.stack.push(jvm.Value.int(v1.value // v2.value))
-            frame.pc += 1
-            return state
-        case jvm.Return(type=jvm.Int()):
-            v1 = frame.stack.pop()
-            state.frames.pop()
-            if state.frames:
-                frame = state.frames.peek()
-                frame.stack.push(v1)
-                frame.pc += 1
-                return state
+    def _push(value):
+        """bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩"""
+        frame.stack.push(value)
+        frame.pc += 1
+        return state
+
+    def _load(index):
+        """
+        bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩
+        """
+        value = frame.locals[index]
+        assert value.type is jvm.Int() or jvm.Boolean()
+        frame.stack.push(value)
+        frame.pc += 1
+        return state
+
+    def _binary_add():
+        """bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩"""
+        v2, v1 = frame.stack.pop(), frame.stack.pop()
+        assert v1.type is jvm.Int(), f"expected int, but got {v1}"
+        assert v2.type is jvm.Int(), f"expected int, but got {v2}"
+        frame.stack.push(jvm.Value.int(v1.value + v2.value))
+        frame.pc += 1
+        return state
+
+    def _binary_sub():
+        """bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩"""
+        v2, v1 = frame.stack.pop(), frame.stack.pop()
+        assert v1.type is jvm.Int(), f"expected int, but got {v1}"
+        assert v2.type is jvm.Int(), f"expected int, but got {v2}"
+        frame.stack.push(jvm.Value.int(v1.value - v2.value))
+        frame.pc += 1
+        return state
+
+    def _binary_div():
+        """bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩"""
+        v2, v1 = frame.stack.pop(), frame.stack.pop()
+        assert v1.type is jvm.Int(), f"expected int, but got {v1}"
+        assert v2.type is jvm.Int(), f"expected int, but got {v2}"
+        if v2.value == 0:
+            return "divide by zero"
+        frame.stack.push(jvm.Value.int(v1.value // v2.value))
+        frame.pc += 1
+        return state
+
+    def _binary_rem():
+        """
+        bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩
+        rem = value1 - (value1 / value2) * value2
+        """
+        v2, v1 = frame.stack.pop(), frame.stack.pop()
+        assert v1.type is jvm.Int(), f"expected int, but got {v1}"
+        assert v2.type is jvm.Int(), f"expected int, but got {v2}"
+        if v2.value == 0:
+            return "divide by zero"
+        vv1, vv2 = v1.value, v2.value
+        frame.stack.push(jvm.Value.int(vv1 - (vv1 // vv2) * vv2))
+        frame.pc += 1
+        return state
+
+    def _binary_mul():
+        """bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩"""
+        v2, v1 = frame.stack.pop(), frame.stack.pop()
+        assert v1.type is jvm.Int(), f"expected int, but got {v1}"
+        assert v2.type is jvm.Int(), f"expected int, but got {v2}"
+
+        frame.stack.push(jvm.Value.int(v1.value * v2.value))
+        frame.pc += 1
+        return state
+
+    def _dup():
+        """bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩"""
+        value = frame.stack.peek()
+        frame.stack.push(value)
+        frame.pc += 1
+        return state
+
+    def _get_static(field):
+        """
+        Pushes the value of the specified static field onto the operand stack.
+        bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ, σ‾, ι‾⟩
+        """
+        value = bytecode.get_static_field(frame.pc, field)
+        frame.stack.push(value)
+        frame.pc += 1
+        return state
+
+    def _if(condition: str, target: int, ifz: bool = False):
+        """
+        1. Pops two values from the operand stack (one in case of [ifz == True]).
+        2. Compares them according to the condition (with 0 in case of [ifz == True]).
+        3. Jumps to target instruction if condition is true.
+        4. Continues to next instruction if condition is false.
+        """
+        if ifz:
+            value = frame.stack.pop()
+            if value.type is jvm.Boolean():
+                v1 = 1 if value.value else 0
             else:
-                return "ok"
-        case a:
-            raise NotImplementedError(f"Don't know how to handle: {a!r}")
+                v1 = value.value
+            v2 = 0
+        else:
+            v2, v1 = frame.stack.pop().value, frame.stack.pop().value
+        match condition:
+            case 'ne':
+                jump = v1 != v2
+            case 'eq':
+                jump = v1 == v2
+            case 'ne':
+                jump = v1 != v2
+            case 'ge':
+                jump = v1 >= v2
+            case 'gt':
+                jump = v1 > v2
+            case 'lt':
+                jump = v1 < v2
+            case 'le':
+                jump = v1 <= v2
+            case unknown:
+                raise NotImplementedError(f"Unknown condition: {unknown!r}")
+
+        frame.pc.offset = target if jump else frame.pc.offset + 1
+        return state
+
+    def _invoke_static(method: jvm.AbsMethodID):
+        new_frame = Frame.from_method(method)
+        new_frame.pc = PC(method, 0)
+
+        for index, param in enumerate(method.extension.params):
+            local = frame.stack.pop()
+
+            # assert isinstance(local.type, type(param)), f'''
+            # Inappropriate argument type: {local.type!r}
+            # (expected {param!r})
+            # '''
+
+            new_frame.locals[index] = local
+
+        state.frames.push(new_frame)
+        return state
+
+    def _return(return_type: jvm.Type | None):
+        """
+        bc ⊢ ⟨λ, σ, ι⟩ → ⟨λ', σ', ι'⟩
+
+        1. Returns control to the invoker of the current method, if present.
+        2. If type is present, returns a value of that type to invoker.
+        3. If type is None (void return), returns no value.
+        """
+        state.frames.pop()
+        if state.frames:
+            new_frame = state.frames.peek()
+
+            if return_type is not None:
+                value = frame.stack.pop()
+
+                # assert value.type is return_type, f'Return type mismatch {return_type} {value.type}'
+
+                new_frame.stack.push(value)
+
+            new_frame.pc += 1
+            return state
+        return "ok"
+
+    def _new(classname: jvm.ClassName):
+        match classname.name:
+            case 'java/lang/AssertionError':
+                return 'assertion error'
+            case _:
+                raise NotImplementedError(f"Unknown classname: {classname!r}")
+
+    def _new_array(array_type: jvm.Type):
+        assert array_type is jvm.Int(), f'NewArray {array_type} not handled'
+
+        size = frame.stack.pop()
+        assert size.type is jvm.Int(), 'Size must be of type Int'
+
+        array = [0 for _ in range(size.value)]
+
+        # TODO extract
+        ref = max(state.heap.keys()) + 1 if len(state.heap.keys()) > 0 else 0
+        state.heap[ref] = jvm.Value.array(array_type, array)
+
+        frame.stack.push(jvm.Value.int(ref))
+        frame.pc += 1
+        return state
+
+    def _store(index: int):
+        value = frame.stack.pop()
+        frame.locals[index] = value
+
+        frame.pc += 1
+        return state
+
+    def _array_store(array_type: jvm.Type):
+        value, index, ref = frame.stack.pop(), frame.stack.pop(), frame.stack.pop()
+
+        if ref.value is None:
+            return 'null pointer'
+
+        assert ref.type is jvm.Int(), f'Array ref type mismatch {ref!r}'
+        assert index.type is jvm.Int(), f'Index type mismatch {index.type}'
+        assert value.type is array_type, f'Value type mismatch {value.type}'
+
+        old_array = state.heap[ref.value]
+
+        if index.value >= len(old_array.value):
+            return 'out of bounds'
+
+        new_array = list(old_array.value)
+        new_array[index.value] = value.value
+
+        state.heap[ref.value] = jvm.Value.array(array_type, new_array)
+
+        frame.pc += 1
+        return state
+
+    def _array_load(array_type: jvm.Type):
+        index, ref = frame.stack.pop(), frame.stack.pop()
+
+        assert ref.type is jvm.Int(), f'Array ref type mismatch {ref!r}'
+        assert index.type is jvm.Int(), f'Index type mismatch {index.type}'
+
+        array = list(state.heap[ref.value].value)
+        if index.value >= len(array):
+            return 'out of bounds'
+
+        value = array[index.value]
+
+        match array_type:
+            case jvm.Int():
+                frame.stack.push(jvm.Value.int(value))
+            case jvm.Char():
+                frame.stack.push(jvm.Value.int(ord(value)))
+            case _:
+                raise NotImplementedError(f"Unknown array type: {array_type}")
+
+        frame.pc += 1
+        return state
+
+    def _goto(target: int):
+        frame.pc.offset = target
+        return state
+
+    def _load(index: int):
+        value = frame.locals[index]
+        frame.stack.push(value)
+
+        frame.pc += 1
+        return state
+
+    def _array_length():
+        ref = frame.stack.pop()
+
+        if ref.value is None:
+            return 'null pointer'
+
+        array = state.heap[ref.value]
+        length = len(array.value)
+
+        frame.stack.push(jvm.Value.int(length))
+
+        frame.pc += 1
+        return state
+
+    def _incr(index: int, amount: int):
+        assert frame.locals[index].type is jvm.Int(), 'Incr type mismatch'
+
+        old_value = frame.locals[index].value
+        new_value = old_value + amount
+
+        frame.locals[index] = jvm.Value.int(new_value)
+
+        frame.pc += 1
+        return state
+
+    def _cast(from_: jvm.Type, to_: jvm.Type):
+        match from_, to_:
+            case jvm.Int(), jvm.Short():
+                value = frame.stack.pop()
+                casted = jvm.Value(jvm.Short(), value.value)
+                frame.stack.push(casted)
+            case _:
+                raise NotImplementedError(f'From {from_} To {to_} not handled')
+
+        frame.pc += 1
+        return state
+
+    logger.info(f"Bytecode[{frame.pc}]: {bytecode[frame.pc]}")
+    logger.info(f"Op Stack[{frame.stack}]")
+    match bytecode[frame.pc]:
+        case jvm.Push(value=v): return _push(value=v)
+        case jvm.Load(type=jvm.Int(), index=n): return _load(index=n)
+        case jvm.Binary(type=jvm.Int(), operant=jvm.BinaryOpr.Add): return _binary_add()
+        case jvm.Binary(type=jvm.Int(), operant=jvm.BinaryOpr.Sub): return _binary_sub()
+        case jvm.Binary(type=jvm.Int(), operant=jvm.BinaryOpr.Div): return _binary_div()
+        case jvm.Binary(type=jvm.Int(), operant=jvm.BinaryOpr.Rem): return _binary_rem()
+        case jvm.Binary(type=jvm.Int(), operant=jvm.BinaryOpr.Mul): return _binary_mul()
+        case jvm.Incr(index=i, amount=a): return _incr(index=i, amount=a)
+        case jvm.Dup(words=1): return _dup()
+        case jvm.Get(static=True, field=f): return _get_static(field=f)
+        case jvm.Return(type=t): return _return(return_type=t)
+        case jvm.If(condition=c, target=t): return _if(condition=c, target=t)
+        case jvm.Ifz(condition=c, target=t): return _if(condition=c, target=t, ifz=True)
+        case jvm.New(classname=cn): return _new(classname=cn)
+        case jvm.Store(type=_, index=i): return _store(index=i)
+        case jvm.NewArray(type=t, dim=1): return _new_array(array_type=t)
+        case jvm.ArrayStore(type=t): return _array_store(array_type=t)
+        case jvm.ArrayLoad(type=t): return _array_load(array_type=t)
+        case jvm.ArrayLength(): return _array_length()
+        case jvm.InvokeStatic(method=m): return _invoke_static(method=m)
+        case jvm.Goto(target=t): return _goto(target=t)
+        case jvm.Load(type=jvm.Reference(), index=i): return _load(index=i)
+        case jvm.Cast(from_=f, to_=t): return _cast(from_=f, to_=t)
+
+        case unknown:
+            raise NotImplementedError(f"Don't know how to handle: {unknown!r}")
 
 
-frame = Frame.from_method(methodid)
-for i, v in enumerate(input.values):
-    frame.locals[i] = v
+def configure_logger():
+    """Configures the logger with a custom format."""
+    logger.remove()
+    logger.add(sys.stderr, format="[{level}] {message}")
 
-state = State({}, Stack.empty().push(frame))
 
-for x in range(1000):
-    state = step(state)
-    if isinstance(state, str):
-        print(state)
-        break
-else:
-    print("*")
+def generate_initial_frame(method_id: jvm.AbsMethodID, method_input: Input) -> tuple[Frame, dict[int, jvm.Value]]:
+    """Generates the initial frame from the given method id and method input"""
+    initial_frame = Frame.from_method(method_id)
+    heap = {}
+
+    for index, value in enumerate(method_input.values):
+        # match value:
+        #     case jvm.Value(type=jvm.Boolean(), value=value):
+        #         local = jvm.Value.int(1 if value else 0)
+        #     case jvm.Value(type=jvm.Int(), value=value):
+        #         local = value
+        #     case _:
+        #         assert False, f"Do not know how to handle {value}"
+        local = wrap_value(value.value)
+        if isinstance(local.type, jvm.Array):
+            ref = len(heap.values())
+            heap[ref] = local
+            initial_frame.locals[index] = jvm.Value.int(ref)
+        else:
+            initial_frame.locals[index] = local
+
+    logger.debug(f"Initial frame local variable {initial_frame.locals}")
+    logger.debug(f"Heap {heap}")
+    return initial_frame, heap
+
+
+if __name__ == "__main__":
+    configure_logger()
+
+    mid, minput = jpamb.getcase()
+    bc = Bytecode(jpamb.Suite(), {})
+    initial_frame, heap = generate_initial_frame(mid, minput)
+    state = State(heap, Stack.empty().push(initial_frame))
+
+    for x in range(100000):
+        state = step(state, bc)
+        if isinstance(state, str):
+            print(state)
+            break
+    else:
+        print("*")
+
+    # state = step(state, bc)
+    # while isinstance(state, str):
+    #     print(state)
+    #     state = step(state, bc)
