d7dc0ca80ca232a2a92449443fe6e03418743ece
Christian Gram Kalhauge <chrg@dtu.dk>
2026-08-28 13:55:21 +0200

    Remove more tests

--- solutions/basic/tests/test_basic.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def test_expected():
-    subprocess.run(
-        "jpamb --workdir ../.. test --fail-fast basic > tests/expected/basic",
-        shell=True,
-        check=True,
-    )
